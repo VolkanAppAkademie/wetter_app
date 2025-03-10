a1630d3e0b4250dcbd2de36116e6365af8bf124e
VolkanAppAkademie <supafly_69@gmx.de>
2025-03-10 17:04:17 +0100

    switched to stateful widget and created init and setState

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,27 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  late Future<double> _temperature;
+
+  @override
+  void initState() {
+    super.initState();
+    _temperature = WeatherService.fetchCurrentTemperature();
+  }
+
+  void _refreshTemperature() {
+    setState(() {
+      _temperature = WeatherService.fetchCurrentTemperature();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,20 +34,17 @@ class MainApp extends StatelessWidget {
         appBar: AppBar(title: const Text('Wetter App')),
         body: Center(
           child: FutureBuilder<double>(
-            future: WeatherService.fetchCurrentTemperature(),
+            future: _temperature,
             builder: (context, snapshot) {
               if (snapshot.hasData) {
                 return Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
+                    Text('Berlin:'),
                     Text('Aktuelle Temperatur: ${snapshot.data}°C'),
-                    const SizedBox(
-                        height: 20), // Abstand zwischen Text und Button
+                    const SizedBox(height: 20),
                     TextButton(
-                      onPressed: () {
-                        // Hier kannst du die Aktion definieren, z. B. die Wetterdaten erneut abrufen
-                        print("Button gedrückt");
-                      },
+                      onPressed: _refreshTemperature,
                       child: const Text('Aktualisieren'),
                     ),
                   ],
